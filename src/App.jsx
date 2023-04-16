0380c85eeee10e6e04dd3ce87c05edd590817922
uli <aldoulisesbc@gmail.com>
2023-04-16 04:39:12 -0600

    preject update 1/2

--- src/App.jsx
+++ src/App.jsx
@@ -47,21 +47,20 @@ function App() {
         <div className="border-[#8EFF8B] border-[1px]  flex max-w-[900px]  m-auto desktop:mr-0 desktop:w-[40%]">
           <input
             id="locationId"
-            type="text"
+            type="number"
             placeholder="Type a location Id..."
-            className="outline-none w-[70%] bg-transparent text-white/30 p-3"
+            className="outline-none w-[70%] bg-transparent text-white/30 p-3 caret-[#8EFF8B] focus:text-white"
           />
-          <button className="w-[30%] border-[#8EFF8B] border-[1px] bg-[#8EFF8B]/50 text-white py-2  ">
+          <button className="w-[30%] border-[#8EFF8B] border-[1px] bg-[#8EFF8B]/50 text-white py-2 hover:bg-[#8EFF8B] ">
             Search <i className="inline bx bx-search"></i>
           </button>
         </div>
-        { !location ? <h2 className="text-[#8EFF8B] text-[20px] font-medium block text-center mt-10 ">Welcome to the crazy universe!</h2> : <Location className=""  location={location} />}
+        { !location ? <h2 className=" text-[#8EFF8B] text-[20px] font-medium block text-center mt-10  ">Welcome to the crazy universe!</h2> : <Location className="desktop:absolute desktop:top-[40%]"  location={location} />}
         
       </form>
       </div>
 <div className="  left-0 right-0 min-h-screen min-w-full bg-[url('/public/bg/bg-body.png')] bg-body bg-center bg-repeat-y pt-24 pb-24 
  desktop:bg-none desktop:absolute  desktop:min-h-0 desktop:min-w-0 desktop:overflow-y-scroll desktop:h-[50vh] desktop:w-[600px] desktop:top-[28%]  desktop:pt-0 desktop:pb-0 desktop:m-auto">
-      
       
       <ResidentList  location={location} />
       </div>
--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Location({location}) {
   return (
-    <div className="text-white  text-center py-10 desktop:hidden" >
+    <div className="text-white  text-center py-10 desktop:absolute desktop:top-[40%] desktop:min-w-[100px] desktop:max-w-[150px] desktop:left-[5%]  desktop:m-auto" >
       <h2 className='text-[25px] font-light'>Location:<span className=' font-semibold text-[#8EFF8B]'>{location?.name}</span></h2>
       <ul className=' font-light '>
         <li className='text-[20px]'>Type: <span className='font-semibold'>{location?.type}</span></li>
